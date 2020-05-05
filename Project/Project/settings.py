64d6e2447d2ec22ec8e43e594d31ad364b50fe1f
Margret Soley <margretmsk@gmail.com>
2020-05-05 12:31:15 +0000

    Starting with connecting the DB

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'llcxcvgx',
+        'USER': 'llcxcvgx',
+        'PASSWORD': 'Ow-P_LpFYLqtw4SopEsUuMxNsSJxMysE',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
