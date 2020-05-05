58207340607bfd1c3ac4195768519d495fda7a76
unknown <johannc18@ru.is>
2020-05-05 16:40:20 +0000

    Profile page started: image and text

--- Project/captain_console/views.py
+++ Project/captain_console/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'captain/index.html')
+
+def profile(request):
+    return render(request, 'captain/profile.html')
