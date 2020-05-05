cea0294f4354790ffa8da3e51cecf7dee18f73e0
Margret Soley <margretmsk@gmail.com>
2020-05-05 18:32:10 +0000

    Dividing products into consoles and games

--- Project/captain_console/urls.py
+++ Project/captain_console/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="captain_console_index"),
+    path('consoles/', views.index, kwargs={'category': 1}, name="category_consoles"),
+    path('games/', views.index, kwargs={'category': 2}, name="category_games"),
     path('profile/', views.profile, name="profile_page")
-]
+]   # path(route, view, kwargs=None, name=None)
--- Project/captain_console/views.py
+++ Project/captain_console/views.py
@@ -4,19 +4,12 @@ from django.http import HttpResponse
 from captain_console.models import Product, ProductImage
 
 
-def index(request):
-    # product = [{
-    #     'id': p.id,
-    #     'name': p.name,
-    #     'description': p.description,
-    #     'category': p.category,
-    #     'amount': p.amount,
-    #     'price':p.price,
-    #     'firstImage': p.productimage_set.first().image
-    # } for p in Product.objects.filter(category__icontains='console')
-    # ]
-    # product = list(Product.objects.filter(category__icontains='console'))
-    # return JsonResponse({'data':captain})
+def index(request, **kwargs):
+    if 'category' in kwargs:
+        cat = kwargs['category']
+        product = list(Product.objects.filter(category=cat))
+        context = {'products': product}
+        return render(request, 'captain/index.html', context)
     context = {'products': Product.objects.all()}
     return render(request, 'captain/index.html', context)
 
