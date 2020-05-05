d0ce440f89769393c462146614c45a58dd93def8
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2020-05-05 16:16:12 +0000

    added product detail page

--- Project/captain_console/views.py
+++ Project/captain_console/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+# adding the modules needed
+from captain_console.models import Product
 
 # Create your views here.
 def index(request):
     return render(request, 'captain/index.html')
+
+# fetches the product using a dynamic parameter like
+# /captain/product/1
+def get_product_by_id(request, id):
+    return render(request, 'captain/product_details.html', {
+        'product': get_object_or_404(Product, pk=id)
+    })
