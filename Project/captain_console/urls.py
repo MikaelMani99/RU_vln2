5e9fedefc954201c6acf339f537608d5cca1969a
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2020-05-06 12:14:51 +0000

    fixed so it goes straight to product/id instead of e.g. console/product/id

--- Project/captain_console/urls.py
+++ Project/captain_console/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path('games/', views.index, kwargs={'category': 2}, name="category_games"),
     path('accessories/', views.index, kwargs={'category': 3}, name="category_accessories"),
     path('product/<int:id>', views.get_product_by_id, name="product_details"),
-    path('consoles/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: find a better solution to this.
-    path('games/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: and this.
-    path('accessories/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: also this.
     path('profile/<int:id>', views.get_profile_by_id, name="profile_page")
 ]   # path(route, view, kwargs=None, name=None)
