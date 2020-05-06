82144341a8a0bae9837dec82d540d510a40e1e63
Margret Soley <margretmsk@gmail.com>
2020-05-06 11:48:15 +0000

    Adding accessories category, and connecting details page

--- Project/captain_console/urls.py
+++ Project/captain_console/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.index, name="captain_console_index"),
     path('consoles/', views.index, kwargs={'category': 1}, name="category_consoles"),
     path('games/', views.index, kwargs={'category': 2}, name="category_games"),
+    path('accessories/', views.index, kwargs={'category': 3}, name="category_accessories"),
     path('product/<int:id>', views.get_product_by_id, name="product_details"),
+    path('consoles/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: find a better solution to this.
+    path('games/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: and this.
+    path('accessories/product/<int:id>', views.get_product_by_id, name="product_details"), # TODO: also this.
     path('profile/<int:id>', views.get_profile_by_id, name="profile_page")
 ]   # path(route, view, kwargs=None, name=None)
