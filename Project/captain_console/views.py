35e68a1dea6ad9900628b3416b0e66138bc56125
Margret Soley <margretmsk@gmail.com>
2020-05-06 14:04:44 +0000

    Added search for all categorie pages and home

--- Project/captain_console/views.py
+++ Project/captain_console/views.py
@@ -4,13 +4,36 @@ from django.http import HttpResponse
 from captain_console.models import Product, ProductImage, User
 
 def index(request, **kwargs):
+    product_ls = []
+    get_all = True
+    if 'search_filter' in request.GET:
+        product_ls = filter_name_contains(request.GET.get('search_filter'))
+        get_all = False
     if 'category' in kwargs:
-        cat = kwargs['category']
-        product = list(Product.objects.filter(category=cat))
-        context = {'products': product}
-        return render(request, 'captain/index.html', context)
-    context = {'products': Product.objects.all()}
+        if not product_ls:  # If product list is empty
+            product_ls = filter_by_category(kwargs['category'])
+            get_all = False
+        else:
+            cat = kwargs['category']
+            product_ls = filter_through_ls(product_ls, cat)
+            get_all = False
+    if get_all:  # If product list is empty and shouldn
+        product_ls = list(Product.objects.all())
+    context = {'products': product_ls}
     return render(request, 'captain/index.html', context)
+
+def filter_name_contains(filter):
+    return list(Product.objects.filter(name__icontains=filter))
+
+def filter_by_category(category):
+    return list(Product.objects.filter(category=category))
+
+def filter_through_ls(ls, filter):
+    new_ls = []
+    for p in ls:
+        if p.category.id == filter:
+            new_ls.append(p)
+    return new_ls
 
 # fetches the product using a dynamic parameter like
 # /captain/product/1
