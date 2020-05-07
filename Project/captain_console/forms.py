3f6a538ce823601f49b877cdadc9f6a36e189137
Margret Soley <margretmsk@gmail.com>
2020-05-07 22:03:02 +0000

    Fixed order by, added filter by category

--- Project/captain_console/forms.py
+++ Project/captain_console/forms.py
@@ -1,11 +1,19 @@
 from django import forms
+from captain_console.models import ProductCategory
 
 ORDER = [
     ('name', 'Name'),
     ('price', 'Price'),
     ]
-class CHOICES(forms.Form):
+
+class OrderFilter(forms.Form):
+    categories = ProductCategory.objects.all()
+    filter_choices = [(str(c.id), c.name) for c in categories]
+    filter_choices.insert(0, ('0', 'Category'))
+
     ORDER = forms.CharField(widget=forms.RadioSelect(choices=ORDER))
+    FILTER = forms.CharField(widget=forms.Select(choices=filter_choices))
+
     class Media:
         css = {
             'all': ('main.css',)
--- Project/captain_console/views.py
+++ Project/captain_console/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 # adding the modules needed
-from captain_console.models import Product, ProductImage, User
-from .forms import CHOICES
+from captain_console.models import Product, ProductImage, User, ProductCategory
+from .forms import OrderFilter
+
 
 def index(request, **kwargs):
     if 'search_filter' in request.GET:
@@ -14,27 +15,28 @@ def index(request, **kwargs):
     context = {'products': product_ls}
     return render(request, 'captain/index.html', context)
 
+
 def search_page(request):
     search_filter = request.GET.get('search_filter')
-    form = CHOICES(request.POST)
+    products = Product.objects.filter(name__icontains=search_filter)
+    form = OrderFilter(request.POST)
     if form.is_valid():
-        selected = form.cleaned_data.get("ORDER")
-        product_ls = list(Product.objects.filter(name__icontains=search_filter).order_by(selected))
-    else:
-        product_ls = list(Product.objects.filter(name__icontains=search_filter))
-    context = {'products': product_ls, 'form': form}
+        selected_filter = form.cleaned_data.get("FILTER")
+        if is_valid_query_param(selected_filter) and selected_filter != 'Category':
+            products = products.filter(category=selected_filter)
+        selected_order = form.cleaned_data.get("ORDER")
+        if is_valid_query_param(selected_order):
+            products = products.order_by(selected_order)
+    context = {
+                'products': products,
+                'form': form,
+    }
     return render(request, 'captain/search_page.html', context)
-    # render(request, '/captain/search_page.html', context)
 
-def filter_by_category(category):
-    return list(Product.objects.filter(category=category))
 
-def filter_through_ls(ls, filter):
-    new_ls = []
-    for p in ls:
-        if p.category.id == filter:
-            new_ls.append(p)
-    return new_ls
+def is_valid_query_param(param):
+    return param != '' and param is not None
+
 
 # fetches the product using a dynamic parameter like
 # /captain/product/1
@@ -42,6 +44,7 @@ def get_product_by_id(request, id):
     return render(request, 'captain/product_details.html', {
         'product': get_object_or_404(Product, pk=id)
     })
+
 
 def get_profile_by_id(request, id):
 
