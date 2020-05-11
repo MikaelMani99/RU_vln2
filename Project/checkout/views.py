0a05086a2be8d00182ce9873cf57de6c5fc25a3c
Eiki47 <eiki@live.com>
2020-05-11 02:18:35 +0000

    Updates on cart functionality

--- Project/checkout/views.py
+++ Project/checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import time
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from product.models import Product, ProductImage, ProductCategory, ProductType
+from chest.models import Cart, CartItem
+from checkout.models import Order
 
-# Create your views here.
+
+def contact_info(request):
+    try:
+        cart_id = request.session['id_of_cart']
+        cart = Cart.objects.get(id=cart_id)
+        print (cart)
+    except:
+        cart_id = None
+        print ("NONE")
+        return HttpResponseRedirect("/chest/")
+
+    new_order, created = Order.objects.get_or_create(cart=cart)
+    if created:
+        new_order.order_id = str(time.time())
+        new_order.save()
+    return render(request, 'captain/checkout_contact_info.html', {})
