3bec8cc792aecf83a635630fa056abf7cf34fea1
unknown <johannc18@ru.is>
2020-05-15 20:29:07 +0000

    error handling for 404 and 500 errors

--- Project/Project/views.py
+++ Project/Project/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect
+from django.shortcuts import render
 
-def view_404(request, exception=None):
-    # make a redirect to homepage
-    return redirect('/')
+def view_404(request, *args, **argv):
+    return render(request, '404.html', {
+        'user': request.user
+    })
+
+def view_500(request, *args, **argv):
+    return redirect('500.html')
