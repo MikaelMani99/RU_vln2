f0513848df105a37d7b862231b3205a9771c07fc
unknown <johannc18@ru.is>
2020-05-15 20:55:29 +0000

    500 error redirect fixed

--- Project/Project/views.py
+++ Project/Project/views.py
@@ -7,4 +7,6 @@ def view_404(request, *args, **argv):
     })
 
 def view_500(request, *args, **argv):
-    return redirect('500.html')
+    return render(request, '500.html', {
+        'user': request.user
+    })
